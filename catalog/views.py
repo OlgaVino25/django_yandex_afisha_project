7d26a98947be5f709528850fdd64753c4cb34068
OlgaVino25 <olgavinokyrova25@yandex.ru>
2025-11-19 15:44:01 +0300

    Move data from template to view

--- catalog/views.py
+++ catalog/views.py
@@ -1,6 +1,43 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from django.http import JsonResponse
+from catalog.models import Place
+import json
 
-# Create your views here.
 
 def home(request):
-    return render(request, "map.html")
+    places = Place.objects.all()
+
+    features = []
+    for place in places:
+        lng = float(place.lng)
+        lat = float(place.lat)
+
+        feature = {
+            "type": "Feature",
+            "geometry": {"type": "Point", "coordinates": [lng, lat]},
+            "properties": {
+                "title": place.title,
+                "placeId": place.id,
+                "detailsUrl": f"/place/{place.id}/",
+            },
+        }
+        features.append(feature)
+
+    geojson_data = {"type": "FeatureCollection", "features": features}
+    geojson_str = json.dumps(geojson_data, ensure_ascii=False)
+
+    return render(request, "map.html", {"geojson_data": geojson_str})
+
+
+def place_detail(request, place_id):
+    place = get_object_or_404(Place, id=place_id)
+
+    place_data = {
+        "title": place.title,
+        "imgs": [img.image.url for img in place.images.all()],
+        "description_short": place.description_short,
+        "description_long": place.description_long,
+        "coordinates": {"lng": float(place.lng), "lat": float(place.lat)},
+    }
+
+    return JsonResponse(place_data)
--- where_to_go/urls.py
+++ where_to_go/urls.py
@@ -25,6 +25,7 @@ from catalog import views
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("", views.home, name="home"),
+    path("place/<int:place_id>/", views.place_detail, name="place_detail"),
 ]
 
 if settings.DEBUG:
